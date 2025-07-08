e168f720fe6ee853a0712ffc9a9be27790da9db5
joaquim.org <joaquim.org@gmail.com>
2025-07-08 15:42:43 +0100

    fix PWA

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,14 @@ export default defineConfig({
         theme_color: '#1976d2',
         icons: [
           {
-            src: 'fastpos-logo.svg',
+            src: 'fastpos-192.png',
             sizes: '192x192',
-            type: 'image/svg+xml'
+            type: 'image/png'
+          },
+          {
+            src: 'fastpos-512.png',
+            sizes: '512x512',
+            type: 'image/png'
           }
         ],
         start_url: '/',
