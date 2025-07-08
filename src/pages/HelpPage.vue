6a12d9eeada37cd0cfea7b395b553e9ae4373b4c
joaquim.org <joaquim.org@gmail.com>
2025-07-08 17:14:24 +0100

    Add help page and navigation

---
+++ src/pages/HelpPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card class="mx-auto my-4" max-width="480" elevation="5">
+    <v-card-title class="text-h6 text-center">Ajuda</v-card-title>
+    <v-card-text>
+      <p>Esta aplicação permite gerir vendas de forma simples e rápida. Abaixo encontram-se as principais funcionalidades:</p>
+      <v-list density="compact" class="bg-transparent">
+        <v-list-item>Gestão de produtos: criar, editar e remover produtos, definindo o seu preço.</v-list-item>
+        <v-list-item>Registo de vendas com múltiplos produtos, cálculo do total e do troco.</v-list-item>
+        <v-list-item>Resumo do dia com totais por produto e opção de fechar o dia.</v-list-item>
+        <v-list-item>Aplicação PWA que funciona offline e guarda os dados localmente.</v-list-item>
+      </v-list>
+      <p class="mt-2">Utilize o menu superior para navegar entre as secções. Em &quot;Registar Venda&quot; adiciona produtos à venda, em &quot;Produtos&quot; gere a lista de produtos disponíveis e em &quot;Resumo do Dia&quot; consulta o total das vendas.</p>
+    </v-card-text>
+  </v-card>
+</template>
+<script setup>
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import SaleRegisterPage from '../pages/SaleRegisterPage.vue'
 import ProductManagerPage from '../pages/ProductManagerPage.vue'
 import SalesSummaryPage from '../pages/SalesSummaryPage.vue'
+import HelpPage from '../pages/HelpPage.vue'
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: SaleRegisterPage },
     { path: '/produtos', component: ProductManagerPage },
-    { path: '/resumo', component: SalesSummaryPage }
+    { path: '/resumo', component: SalesSummaryPage },
+    { path: '/ajuda', component: HelpPage }
   ]
 })
